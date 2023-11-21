30ddc7ac98dd17488e3f34b4eac41987a62c0966
Alexey <alexei.solo@outlook.com>
2023-11-21 03:08:42 +0200

    add minor fix

--- router/auth-routers.js
+++ router/auth-routers.js
@@ -2,13 +2,13 @@ const express = require('express');
 const AuthController = require('../controllers/auth-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
 const { loginValidation, registrationValidation, refreshValidation } = require('../validations/auth-validations');
-const { validate } = require('../validations/validation');
+const validationMiddleware = require('../middlewares/validation-middleware');
 const router = express.Router();
 
 
-router.post('/login', loginValidation, validate, AuthController.login);
-router.post('/register', registrationValidation, validate, AuthController.register);
-router.post('/refresh', refreshValidation, validate, AuthController.refresh)
+router.post('/login', loginValidation, validationMiddleware, AuthController.login);
+router.post('/register', registrationValidation, validationMiddleware, AuthController.register);
+router.post('/refresh', refreshValidation, validationMiddleware, AuthController.refresh)
 
 //router.get('/protected', authMiddleware, AuthController.protected);
 
